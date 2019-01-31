c083034d6c9262b03bfd81ca25c97ba41a920d9f
Miss Keira <kaw504@nyu.edu>
2019-01-31 15:47:08 -0500

    update DESeq

--- data/DESeqFns.R
+++ data/DESeqFns.R
@@ -1,6 +1,9 @@
 get.dds <- function(
-  dat,expDesign,design,path='.',comparisons=NULL,cooksCutoff=T,parallel=T,alpha=.1,independentFiltering=T,...
+  dat,expDesign,design,path='.',comparisons=NULL,cooksCutoff=T,
+  parallel=T,alpha=.1,independentFiltering=T,...
 ){
+  # wrapper function for DESeq2 which saves the dataset and results object to a specified directory
+  # comparisons should be a list of comparisons passed to res.list
   require(DESeq2)
   expDesign[expDesign==T] <- "yes"
   expDesign[expDesign==F] <- "no"
@@ -17,19 +20,23 @@ get.dds <- function(
   dir.csv(normcts,'counts_norm',path)
   
   save(dds,file = mkdate('dds','Rdata',path))
+  res <- NULL
   if(!is.null(comparisons)){
-    return(res.list(
+    res <- res.list(
       dds,comparisons,path,
       cooksCutoff=cooksCutoff,parallel=parallel,alpha=alpha,
       independentFiltering=independentFiltering
-    ))
+    )
   }
+  return(list(dds=dds,res=res))
 }
 
 res.list <- function(
   dds,comparisons,path='.',format = "DataFrame",alpha = .1,
   cooksCutoff=T,parallel=T,independentFiltering=T
 ){
+  # accepts a DESeqDataSet and a list of comparisons
+  # applies results to the DESeqDataSet for each comparison in comparisons
   require(DESeq2)
   res <-  mapply(
     results,list(dds),comparisons,
@@ -46,6 +53,17 @@ res.list <- function(
 
 
 is.sig <- function(x,lfc=1,p=0.05,tail='both',na.as.false=T) {
+  # returns a logical vector of which rows in x are significant
+  # x     a data.frame with columns p.adjust and log2FoldChange
+  # lfc   log2FoldChange cutoff 
+  #       If lfc is a vector of length 2, the first element is taken as the lower limit 
+  #       and the second element is taken as the upper limit.
+  # p     FDR cutoff
+  # tail  may be one of "both","upper",or "lower".
+  #       If "both", returns TRUE if x > lfc or x < -lfc
+  #       If "upper",returns TRUE if x > lfc
+  #       If "lower", returns TRUE if x < -lfc
+  # na.as.false logical indicating whether to return FALSE if is.na(x)
   res <- x$padj<p
   if(length(lfc)==2){
     res <- (x$log2FoldChange<lfc[1]|x$log2FoldChange>lfc[2])&res
@@ -62,8 +80,16 @@ is.sig <- function(x,lfc=1,p=0.05,tail='both',na.as.false=T) {
   return(res)
 }
 
+# wrapper function for converting is.sig to numeric indices
 which.sig <- function(x,lfc=1,p=0.05,tail='both') which(is.sig(x,lfc,p,tail))
+
+# wrapper function for subsetting x by is.sig(x)
 sig.sub <- function(x,lfc=1,p=0.05,tail='both') x[which(is.sig(x,lfc,p,tail)),]
+
+# applies is.sig to a list of data.frames
+# returns the union of all significant rows in any data.frame in the list
+# lfc may be a list or a vector of the same length as x, 
+# in which case each element in lfc is taken as a separate cutoff
 any.sig <- function(x,lfc=1,p=0.05,tail='both') if((length(lfc)==length(x)&is.character(tail))|is.list(lfc)){
   apply(mapply(is.sig,x,lfc,p,tail),1,any,na.rm=T)
 }else {
