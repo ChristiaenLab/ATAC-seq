dafedb5031f52ead21031d697923d7bbeb93ab40
Miss Keira <kaw504@nyu.edu>
2019-05-29 10:26:42 -0400

    forge genome

--- BSgenome.Cintestinalis.KH.JoinedScaffold/forgeGenome.R
+++ BSgenome.Cintestinalis.KH.JoinedScaffold/forgeGenome.R
@@ -2,7 +2,7 @@ library(BSgenome)
 kh <- readLines("JoinedScaffold")
 file.start <- grep('>',kh)
 file.end <- c(file.start[-1]-1,length(kh))
-file.names <- paste0(sub('>','',kh[file.start]),'.fa')
+file.names <- paste0("KH/",sub('>','',kh[file.start]),'.fa')
 mapply(
   function(start,end,name) writeLines(kh[start:end],name),
   file.start,
