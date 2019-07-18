9242eb85f229a9f2263d66db16406fa8cb0bddac
Miss Keira <kaw504@nyu.edu>
2019-07-18 17:50:01 -0400

    update figs

--- chromVarHomer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# data for Fig. 2A
-./rscript.s chromVarHomer.R --peaks daMesp.bed --out mesp --tissue "B7.5" --time "6hpf,10hpf"
-
-# data for Fig. S20
-./rscript.s chromVarHomer.R --peaks denovoCardiacPeaks.bed --out denovoCardiac  --tissue "B7.5"
-./rscript.s chromVarHomer.R --peaks denovoASMpeaks.bed --out denovoASM  --tissue "B7.5"
