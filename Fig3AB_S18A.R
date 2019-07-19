fa7641f843d3e628d6dec34c041e083ca9ece8ae
Miss Keira <kaw504@nyu.edu>
2019-07-19 15:41:53 -0400

    update figs

--- Fig3AB_S18A.R
+++ Fig3AB_S18A.R
@@ -23,53 +23,6 @@ bg <- apply(bg,2,sum)
 bg <- bg/sum(bg)
 
 motifs <- reduceMotifs(con,F,F,F)
-dir.tab(cbind(
-  ID=names(motifs),
-  TF_Name=name(motifs),
-  GeneID=ID(motifs),
-  do.call(rbind,lapply(tags(motifs),'[',c("Family_Name","Motif_Type","DBID.1")))
-),'motifdat',row.names=F,quote=T)
-
-motif.dup <- setNames(motifs,name(motifs))
-# Fig S10C
-alignMotifs('nkx2_3.fa',motif.dup,bg=bg)
-# Fig S12C
-alignMotifs('hand.fa',motif.dup,bg=bg)
-alignMotifs('handfull.fa',motif.dup,bg=bg)
-# Fig S13C
-alignMotifs('smurf.fa',motif.dup,bg=bg)
-
-# Fig S19A
-ebfscore <- matchMotifs(motifs,ann$peaks[
-    paste0('KhL24.',as.character(c(37:34,31,28,27)))
-],Cintestinalis,bg=bg,out='scores')
-
-ebfscore <- sapply(motifByGene,function(x) apply(motifScores(ebfscore)[,x,drop=F],1,max))
-colnames(ebfscore) <- sapply(motifByGene,'[',1)
-
-plotPeakMatches(
-  ebfscore,
-  'FigS19A',
-  sapply(tags(motifs),'[[',"Family_Name")[colnames(ebfscore)]
-)
-
-# Fig S20C
-t12 <- matchMotifs(
-  motifs,
-  c(setNames(GRanges("KhC7",IRanges(1974923,1975287)),'t12'),ann$peaks[c(
-    # "KhC7.909",
-    "KhC7.914")]),
-  Cintestinalis,bg=bg,out='scores'
-)
-tbxscore <- sapply(motifByGene,function(x) apply(motifScores(t12)[,x,drop=F],1,max))
-colnames(tbxscore) <- sapply(motifByGene,'[',1)
-
-plotPeakMatches(
-  tbxscore,
-  "FigS20C",
-  sapply(tags(motifs),'[[',"Family_Name")[colnames(tbxscore)]
-)
-
 matches <- matchMotifs(motifs,ann$peaks,Cintestinalis,bg=bg)
 sel <- split(names(motifs),ID(motifs))
 
